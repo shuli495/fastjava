1f43fa615e68c1f3a106a55c6aa26ac42c35752d
slwangm <slwangm@isoftstone.com>
2018-01-19 09:47:47 +0800

    获取ip工具类增加real-ip判断

--- src/com/fastjavaframework/util/CommonUtil.java
+++ src/com/fastjavaframework/util/CommonUtil.java
@@ -26,7 +26,10 @@ public class CommonUtil {
 	 * @return IP
 	 */
 	public static String getIp(HttpServletRequest request) {
-		String ip = request.getHeader("x-forwarded-for");
+		String ip = request.getHeader("X-Real-IP");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("x-forwarded-for");
+		}
 		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("Proxy-Client-IP");
 		}
